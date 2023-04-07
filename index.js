27058b5f2817b83d7229b4bcf0c54854ac46d583
Shania Lopez <slopez@fandm.edu>
2023-04-07 16:35:54 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+saturdayFun('skip and hop');
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = '*') {
+    return function (adj = "special") {
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
